040b67584dab6eea08afdb78aac7ccf80b966ff9
xxcoldinme <misha_stonlex@bk.ru>
2023-05-09 20:24:43 +0300

    add file-sessions utils

---
+++ services/src/main/java/me/moonways/bridgenet/api/util/file/session/SessionFlag.java
@@ -0,0 +1,8 @@
+package me.moonways.bridgenet.api.util.file.session;
+
+public enum SessionFlag {
+
+    CREATE_IF_NOT_EXISTS,
+    FLUSH_AFTER_CLOSING,
+    PRELOAD_FILE_CONTENT,
+}
---
+++ services/src/main/java/me/moonways/bridgenet/api/util/file/session/SessionType.java
@@ -0,0 +1,8 @@
+package me.moonways.bridgenet.api.util.file.session;
+
+public enum SessionType {
+
+    WRITE_ONLY,
+    READ_ONLY,
+    READ_AND_WRITE,
+}
