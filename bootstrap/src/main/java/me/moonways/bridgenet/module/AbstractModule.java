72911b39a5ddc58cb93fefdff5a7e971141a450a
xxcoldinme <misha_stonlex@bk.ru>
2023-05-09 16:06:51 +0300

    update AbstractModule data access

--- bootstrap/src/main/java/me/moonways/bridgenet/module/AbstractModule.java
+++ bootstrap/src/main/java/me/moonways/bridgenet/module/AbstractModule.java
@@ -1,22 +1,14 @@
 package me.moonways.bridgenet.module;
 
 import lombok.Getter;
-import me.moonways.bridgenet.command.CommandRegistry;
+import me.moonways.bridgenet.BridgenetControl;
 import me.moonways.bridgenet.dependencyinjection.Inject;
-import me.moonways.bridgenet.event.EventService;
-import me.moonways.bridgenet.scheduler.Scheduler;
 
 @Getter
 public abstract class AbstractModule implements Module {
 
     @Inject
-    private CommandRegistry commandRegistry;
-
-    @Inject
-    private EventService eventService;
-
-    @Inject
-    private Scheduler scheduler;
+    private BridgenetControl bridgenetControl;
 
     @Override
     public void onEnable() {
