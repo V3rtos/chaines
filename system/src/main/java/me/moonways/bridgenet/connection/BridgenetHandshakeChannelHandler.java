42c11f96c140ce69152cfee1b784449e834271cd
SJorkins <h4SKV9Du6XT5KCkyw@D6>
2023-05-16 05:09:45 +0300

    Refactor BridgenetHandshakeChannelHandler.java and add handshake server

--- system/src/main/java/me/moonways/bridgenet/connection/BridgenetHandshakeChannelHandler.java
+++ system/src/main/java/me/moonways/bridgenet/connection/BridgenetHandshakeChannelHandler.java
@@ -2,7 +2,9 @@ package me.moonways.bridgenet.connection;
 
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import me.moonways.bridgenet.api.connection.server.Server;
 import me.moonways.bridgenet.api.connection.server.ServerManager;
+import me.moonways.bridgenet.api.connection.server.type.LobbyServer;
 import me.moonways.bridgenet.api.connection.server.type.VelocityServer;
 import me.moonways.bridgenet.protocol.BridgenetChannel;
 import me.moonways.bridgenet.protocol.message.Message;
@@ -11,6 +13,7 @@ import me.moonways.bridgenet.protocol.message.MessageTrigger;
 import me.moonways.bridgenet.service.inject.Inject;
 import me.moonways.bridgenet.services.connection.server.ServerConnectResponseType;
 import me.moonways.bridgenet.services.connection.server.message.HandshakeResponseMessage;
+import me.moonways.bridgenet.services.connection.server.message.LobbyHandshakeMessage;
 import me.moonways.bridgenet.services.connection.server.message.VelocityHandshakeMessage;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +21,7 @@ import java.net.InetSocketAddress;
 
 @MessageHandler
 @Log4j2
-public class BridgenetChannelHandler {
+public class BridgenetHandshakeChannelHandler {
 
     @Inject
     @Getter
@@ -30,16 +33,31 @@ public class BridgenetChannelHandler {
         String host = message.getHost();
         int port = message.getPort();
 
-        VelocityServer velocityServer = createServer(message.getChannel(), serverName, host, port);
+        VelocityServer velocityServer = createVelocityServer(message.getChannel(), serverName, host, port);
 
+        addServer(message, velocityServer);
+    }
+
+    @MessageTrigger
+    public void handle(LobbyHandshakeMessage message) {
+        String serverName = message.getServerName();
+        String host = message.getHost();
+        int port = message.getPort();
+
+        LobbyServer lobbyServer = createLobbyServer(message.getChannel(), serverName, host, port);
+
+        addServer(message, lobbyServer);
+    }
+
+    private void addServer(@NotNull Message message, @NotNull Server server) {
         try {
-            serverManager.addServer(velocityServer);
+            serverManager.addServer(server);
         } catch (NullPointerException exception) {
             int alreadyConnectedIdentifier = ServerConnectResponseType.ALREADY_CONNECTED.getIdentifier();
 
             writeResponseHandshake(message, alreadyConnectedIdentifier);
 
-            log.error("velocity {} already connected", serverName);
+            log.error("server {} already connected", server.getName());
         }
 
         int successfulConnectedIdentifier = ServerConnectResponseType.ALREADY_CONNECTED.getIdentifier();
@@ -51,7 +69,11 @@ public class BridgenetChannelHandler {
         message.writeResponse(new HandshakeResponseMessage(responseId));
     }
 
-    private VelocityServer createServer(@NotNull BridgenetChannel channel, @NotNull String serverName, @NotNull String host, int port) {
+    private VelocityServer createVelocityServer(@NotNull BridgenetChannel channel, @NotNull String serverName, @NotNull String host, int port) {
         return new VelocityServer(serverName, channel, new InetSocketAddress(host, port));
+    }
+
+    private LobbyServer createLobbyServer(@NotNull BridgenetChannel channel, @NotNull String serverName, @NotNull String host, int port) {
+        return new LobbyServer(serverName, channel, new InetSocketAddress(host, port));
     }
 }
