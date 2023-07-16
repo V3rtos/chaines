ea200d942c6c95282d334e0381229cdd98c769ea
xxcoldinme <misha_stonlex@bk.ru>
2023-07-16 21:36:24 +0300

    done project build bash

--- .scripts/build_project.sh
+++ .scripts/build_project.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+application_jarfile="bridgenet-server.jar"
+rsiconfig_file="rsiconfig.xml";
+injectconfig_file="injectconfig.xml";
+
+mkdir "$BUILD_DIR"
+
+function cleanup() {
+  # shellcheck disable=SC2115
+  rm -f "$BUILD_DIR/$application_jarfile"
+  # shellcheck disable=SC2115
+  rm -f "$BUILD_DIR/$rsiconfig_file"
+  # shellcheck disable=SC2115
+  rm -f "$BUILD_DIR/$injectconfig_file"
+}
+
+function application() {
+  cp -R bootstrap/target/$application_jarfile "$BUILD_DIR"
+  cp -R rsi/src/main/resources/$rsiconfig_file "$BUILD_DIR"
+  cp -R inject/src/main/resources/$injectconfig_file "$BUILD_DIR"
+}
 
 function install() {
   cd "$1"
@@ -8,9 +28,15 @@ function install() {
   cd ../
 }
 
-install api
-install inject
-install rsi
-install protocol
-install rest
-install bootstrap
+cleanup
+# shellcheck disable=SC2054
+declare -a modules_array=("api" "inject" "rsi" "protocol" "rest" "bootstrap")
+
+# shellcheck disable=SC2128
+for module in "${modules_array[@]}"
+do
+  install "$module"
+done
+
+application
+except_code
