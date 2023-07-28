66de2db562f90144d72af446029d5d0c8bad77d3
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2023-07-28 03:55:58 +0300

    fix field injection queue

--- api/src/main/java/me/moonways/bridgenet/api/inject/FieldInjectManager.java
+++ api/src/main/java/me/moonways/bridgenet/api/inject/FieldInjectManager.java
@@ -1,7 +1,9 @@
 package me.moonways.bridgenet.api.inject;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
@@ -18,9 +20,13 @@ public class FieldInjectManager implements Serializable {
 
     private final DependencyContainer container;
 
+    private final Queue<FieldQueueState> queue = new ArrayDeque<>();
+
     public void injectFields(@NotNull Object instance) {
         Set<Class<?>> dependenciesClasses = container.getFoundComponentsTypes();
         Field[] fieldsWithInjectAnnotation = findFieldsWithInjectAnnotation(instance);
+
+        FieldQueueState[] fieldsInQueueByDependency = findFieldsInQueueByDependency(instance.getClass());
 
         for (Field field : fieldsWithInjectAnnotation) {
             Class<?> returnType = field.getType();
@@ -28,6 +34,13 @@ public class FieldInjectManager implements Serializable {
             if (dependenciesClasses.contains(returnType)) {
                 injectDependency(instance, field, container.findInstance(returnType));
             }
+            else {
+                queue.add(new FieldQueueState(field, instance));
+            }
+        }
+
+        for (FieldQueueState fieldQueueState : fieldsInQueueByDependency) {
+            injectDependency(fieldQueueState.instance, fieldQueueState.field, instance);
         }
     }
 
@@ -66,5 +79,18 @@ public class FieldInjectManager implements Serializable {
         }
 
         return list;
+    }
+
+    private FieldQueueState[] findFieldsInQueueByDependency(Class<?> dependencyClass) {
+        return queue.stream()
+            .filter(state -> state.field.getType().isAssignableFrom(dependencyClass))
+            .toArray(FieldQueueState[]::new);
+    }
+
+    @RequiredArgsConstructor
+    private static class FieldQueueState {
+
+        private final Field field;
+        private final Object instance;
     }
 }
