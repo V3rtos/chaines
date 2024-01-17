3d915db3ffa8213d5d0ef92b7cec8871da8de0bb
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2024-01-17 17:10:01 +0300

    try to fix gitlab_ci tests

--- api/src/main/java/me/moonways/bridgenet/api/inject/scanner/controller/DependScannerController.java
+++ api/src/main/java/me/moonways/bridgenet/api/inject/scanner/controller/DependScannerController.java
@@ -7,14 +7,14 @@ import me.moonways.bridgenet.api.inject.factory.ObjectFactory;
 import me.moonways.bridgenet.api.inject.scanner.DependencyScanner;
 import me.moonways.bridgenet.api.inject.scanner.ScannerFilter;
 import org.jetbrains.annotations.NotNull;
-import org.reflections.Configuration;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
+import org.reflections8.Configuration;
+import org.reflections8.Reflections;
+import org.reflections8.scanners.ResourcesScanner;
+import org.reflections8.scanners.SubTypesScanner;
+import org.reflections8.scanners.TypeAnnotationsScanner;
+import org.reflections8.util.ClasspathHelper;
+import org.reflections8.util.ConfigurationBuilder;
+import org.reflections8.util.FilterBuilder;
 
 import java.lang.annotation.Annotation;
 import java.net.URL;
@@ -27,28 +27,20 @@ public class DependScannerController implements ScannerController {
     private final SubTypesScanner subTypesScanner = new SubTypesScanner(false);
     private final ResourcesScanner resourcesScanner = new ResourcesScanner();
 
-    private Set<Class<?>> mergeSets(Set<Set<Class<?>>> setOfSets) {
-        Set<Class<?>> resultSet = new HashSet<>();
-
-        for (Set<Class<?>> internal : setOfSets) {
-            resultSet.addAll(internal);
-        }
-
-        return resultSet;
-    }
-
     private Configuration createConfiguration(ScannerFilter scannerFilter) {
         log.info("Create scanner configuration by {}", scannerFilter);
 
-        List<ClassLoader> classLoadersList = new LinkedList<>();
+        List<ClassLoader> classLoadersList = new ArrayList<>();
 
         classLoadersList.add(ClasspathHelper.contextClassLoader());
         classLoadersList.add(ClasspathHelper.staticClassLoader());
 
         FilterBuilder inputsFilter = new FilterBuilder()
-                .includePackage(scannerFilter.getPackageNames().toArray(new String[0]));
+                .includePackage(scannerFilter.getPackageNames()
+                        .toArray(new String[0]));
 
-        Collection<URL> urls = ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0]));
+        Collection<URL> urls = ClasspathHelper.forClassLoader(
+                classLoadersList.toArray(new ClassLoader[0]));
 
         return new ConfigurationBuilder()
                 .setUrls(urls)
@@ -63,14 +55,15 @@ public class DependScannerController implements ScannerController {
         Reflections reflections = new Reflections(configuration);
 
         Set<Class<? extends Annotation>> annotations = filter.getAnnotations();
-        Set<Set<Class<?>>> setOfSets = new HashSet<>();
+
+        Set<Class<?>> result = new HashSet<>();
 
         for (Class<? extends Annotation> annotation : annotations) {
             Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(annotation);
-            setOfSets.add(typesAnnotatedWith);
+            result.addAll(typesAnnotatedWith);
         }
 
-        return mergeSets(setOfSets);
+        return result;
     }
 
     @Override
