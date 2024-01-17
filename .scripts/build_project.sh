bcfec0d25649d99d25465b962a9e5d05849e8b30
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2024-01-17 16:13:36 +0300

    try to fix gitlab_ci tests

--- .scripts/build_project.sh
+++ .scripts/build_project.sh
@@ -4,7 +4,7 @@ bootstrap_file="bootstrap.xml";
 rsiconfig_file="rsiconfig.xml";
 injectconfig_file="injectconfig.xml";
 
-mvn clean install -N -h
+mvn clean install -N
 mkdir "$BUILD_DIR"
 
 function cleanup() {
