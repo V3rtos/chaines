01c0cdbbe859f042ed64e6a987a90eb4f3f2a4cc
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2024-01-18 19:00:42 +0300

    fix errors

--- services/endpoint/games/src/main/java/me/moonways/endpoint/games/stub/GameStateStub.java
+++ services/endpoint/games/src/main/java/me/moonways/endpoint/games/stub/GameStateStub.java
@@ -7,19 +7,34 @@ import me.moonways.bridgenet.model.games.GameStatus;
 import me.moonways.bridgenet.rsi.endpoint.AbstractEndpointDefinition;
 import org.jetbrains.annotations.NotNull;
 
+import java.rmi.RemoteException;
 import java.util.Objects;
 
 @Getter
-@Builder(toBuilder = true)
 public class GameStateStub extends AbstractEndpointDefinition implements GameState {
 
-    private GameStatus status;
-    private String map;
+    private final GameStatus status;
+    private final String map;
 
-    private int maxPlayers;
-    private int playersInTeam;
-    private int players;
-    private int spectators;
+    private final int maxPlayers;
+    private final int playersInTeam;
+    private final int players;
+    private final int spectators;
+
+    @Builder(toBuilder = true)
+    public GameStateStub(GameStatus status, String map,
+                         int maxPlayers,
+                         int playersInTeam,
+                         int players,
+                         int spectators) throws RemoteException {
+        super();
+        this.status = status;
+        this.map = map;
+        this.maxPlayers = maxPlayers;
+        this.playersInTeam = playersInTeam;
+        this.players = players;
+        this.spectators = spectators;
+    }
 
     @Override
     public boolean checkStatus(@NotNull GameStatus status) {
