d5a90928d67ac6bf236a2a29e915af9ead19efac
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2024-01-18 13:56:08 +0300

    add javadocs for ServersServiceModel

--- services/model/src/main/java/me/moonways/bridgenet/model/servers/ServersServiceModel.java
+++ services/model/src/main/java/me/moonways/bridgenet/model/servers/ServersServiceModel.java
@@ -10,15 +10,47 @@ import java.util.UUID;
 
 public interface ServersServiceModel extends RemoteService {
 
+    /**
+     * Получить список стандартных серверов для входа
+     * игроков или других возможных операций.
+     */
     List<EntityServer> getDefaultServers() throws RemoteException;
 
+    /**
+     * Получить список серверов, которые отвечают за редирект
+     * при падении текущих серверов.
+     */
     List<EntityServer> getFallbackServers() throws RemoteException;
 
+    /**
+     * Получить инициализированный и подключенный сервер
+     * по его уникальному ключевому идентификатору.
+     *
+     * @param uuid - ключ сервера.
+     */
     Optional<EntityServer> getServer(@NotNull UUID uuid) throws RemoteException;
 
+    /**
+     * Получить инициализированный и подключенный сервер
+     * по его само-идентифицированному названию.
+     *
+     * @param serverName - название сервера.
+     */
     Optional<EntityServer> getServer(@NotNull String serverName) throws RemoteException;
 
+    /**
+     * Проверить сервер, подключен ли он к системе по
+     * его уникальному ключевому идентификатору.
+     *
+     * @param uuid - ключ сервера.
+     */
     boolean hasServer(@NotNull UUID uuid) throws RemoteException;
 
+    /**
+     * Проверить сервер, подключен ли он к системе по
+     * его само-идентифицированному названию.
+     *
+     * @param serverName - название сервера.
+     */
     boolean hasServer(@NotNull String serverName) throws RemoteException;
 }
