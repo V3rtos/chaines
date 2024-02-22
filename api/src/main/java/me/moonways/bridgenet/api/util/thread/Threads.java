5207dc93cc6323cdca0b0485487ff7ffdd072823
xxcoldinme <misha_stonlex@bk.ru>
2024-02-22 15:41:21 +0300

    fix other threads errors logging

--- api/src/main/java/me/moonways/bridgenet/api/util/thread/Threads.java
+++ api/src/main/java/me/moonways/bridgenet/api/util/thread/Threads.java
@@ -1,33 +1,73 @@
 package me.moonways.bridgenet.api.util.thread;
 
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
+import me.moonways.bridgenet.api.util.minecraft.ChatColor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 
 @UtilityClass
 public class Threads {
 
+    private final ThreadFactory THREAD_FACTORY = new WrappedThreadsFactory(Executors.defaultThreadFactory());
+
     private final List<ExecutorService> EXECUTOR_SERVICES = new ArrayList<>();
     private final List<Runnable> SHUTDOWN_HOOKS = new ArrayList<>();
 
+    @RequiredArgsConstructor
+    private static class WrappedThreadsFactory implements ThreadFactory {
+
+        private final ThreadFactory handle;
+
+        @Override
+        public Thread newThread(@NotNull Runnable r) {
+            Thread thread = handle.newThread(r);
+            interceptThreadExceptions(thread);
+            return thread;
+        }
+
+        private void interceptThreadExceptions(Thread thread) {
+            thread.setUncaughtExceptionHandler(new WrappedUncaughtExceptionHandler());
+        }
+    }
+
+    @Log4j2
+    private static class WrappedUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        private static final String LOG_MESSAGE = ChatColor.RED + "Thread {} caught an exception that prevented its task from executing correctly: {}";
+
+        @Override
+        public void uncaughtException(Thread thread, Throwable exception) {
+            log.error(LOG_MESSAGE, thread.getName(), exception.toString(), exception);
+        }
+    }
+
     public ExecutorService newCachedThreadPool() {
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newCachedThreadPool(THREAD_FACTORY);
+        pull(executorService);
+        return executorService;
+    }
+
+    public ExecutorService newWorkSteelingPool() {
+        ExecutorService executorService = new ForkJoinPool
+                (Runtime.getRuntime().availableProcessors(),
+                        ForkJoinPool.defaultForkJoinWorkerThreadFactory,
+                        new WrappedUncaughtExceptionHandler(), true);
         pull(executorService);
         return executorService;
     }
 
     public ScheduledExecutorService newSingleThreadScheduledExecutor() {
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(THREAD_FACTORY);
         pull(executorService);
         return executorService;
     }
 
     public ScheduledExecutorService newScheduledThreadPool(int corePoolSize) {
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(corePoolSize);
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(corePoolSize, THREAD_FACTORY);
         pull(executorService);
         return executorService;
     }
