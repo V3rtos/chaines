5d3590fde652ec7cfa4c8a43dae1c5e0c297ba45
xxcoldinme <misha_stonlex@bk.ru>
2024-02-23 13:31:49 +0300

    add javadocs for 'assembly' module

--- assembly/src/main/java/me/moonways/bridgenet/assembly/ResourcesTypes.java
+++ assembly/src/main/java/me/moonways/bridgenet/assembly/ResourcesTypes.java
@@ -12,9 +12,10 @@ public final class ResourcesTypes {
     public static final String MTP_SETTINGS_JSON = "mtp_settings.json";
     public static final String REST_SERVER_XML = "rest_server.xml";
     public static final String RSI_CONFIG_XML = "rsiconfig.xml";
-
-    // class-loader resources with InputStream.
     public static final String DECORATORS_XML = "decorators.xml";
     public static final String RMI_POLICY = "rmi.policy";
+
+    // class-loader resources with InputStream.
+    public static final String LOG4J2_XML = "log4j2.xml";
 
 }
