02966bb66f5db1c6c3d2529872029234ed9f6d32
xxcoldinme <misha_stonlex@bk.ru>
2024-02-27 16:30:46 +0300

    add listening PluginDisableEvent at BridgenetSpigotPlugin

--- connector/spigot/src/main/java/me/moonways/bridgenet/connector/spigot/BridgenetSpigotPlugin.java
+++ connector/spigot/src/main/java/me/moonways/bridgenet/connector/spigot/BridgenetSpigotPlugin.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -40,6 +41,24 @@ public class BridgenetSpigotPlugin extends JavaPlugin implements Listener {
         bridgenet.exportDeviceDisconnect();
     }
 
+    private void bindLoadedPlugins() {
+        for (Plugin plugin : getServer().getPluginManager().getPlugins()) {
+            beansService.bind(plugin);
+        }
+    }
+
+    // ---------------------------------------------- // LISTENING BUKKIT EVENTS // ---------------------------------------------- //
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void handle(PluginEnableEvent event) {
+        beansService.bind(event.getPlugin());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void handle(PluginDisableEvent event) {
+        beansService.unbind(event.getPlugin());
+    }
+
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void handle(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
@@ -50,14 +69,5 @@ public class BridgenetSpigotPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void handle(PluginEnableEvent event) {
-        beansService.bind(event.getPlugin());
-    }
-
-    private void bindLoadedPlugins() {
-        for (Plugin plugin : getServer().getPluginManager().getPlugins()) {
-            beansService.bind(plugin);
-        }
-    }
+    // ---------------------------------------------- // LISTENING BUKKIT EVENTS // ---------------------------------------------- //
 }
