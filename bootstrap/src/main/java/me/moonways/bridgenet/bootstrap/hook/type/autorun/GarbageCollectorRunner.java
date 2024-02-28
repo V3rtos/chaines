1d72d06ca40f42d12b4c959735ed2e60324d650a
xxcoldinme <misha_stonlex@bk.ru>
2024-02-28 13:22:55 +0300

    add logging at GarbageCollectorRunner

--- bootstrap/src/main/java/me/moonways/bridgenet/bootstrap/hook/type/autorun/GarbageCollectorRunner.java
+++ bootstrap/src/main/java/me/moonways/bridgenet/bootstrap/hook/type/autorun/GarbageCollectorRunner.java
@@ -1,11 +1,13 @@
 package me.moonways.bridgenet.bootstrap.hook.type.autorun;
 
+import lombok.extern.log4j.Log4j2;
 import me.moonways.bridgenet.api.util.autorun.persistence.AutoRunner;
 import me.moonways.bridgenet.api.util.autorun.persistence.Runnable;
 import me.moonways.bridgenet.api.util.autorun.persistence.RunningPeriod;
 
 import java.util.concurrent.TimeUnit;
 
+@Log4j2
 @AutoRunner
 @RunningPeriod(period = 1, unit = TimeUnit.MINUTES)
 public class GarbageCollectorRunner {
@@ -13,5 +15,6 @@ public class GarbageCollectorRunner {
     @Runnable
     public void run() {
         Runtime.getRuntime().gc();
+        log.debug("Memory was cleaned automatically");
     }
 }
