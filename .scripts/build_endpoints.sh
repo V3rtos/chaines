e21e2454dbfc1ab99ef4e75785cb6f409778e494
xxcoldinme <misha_stonlex@bk.ru>
2024-02-29 16:57:46 +0300

    try to add 'unit_test' stage for CI/CD

--- .scripts/build_endpoints.sh
+++ .scripts/build_endpoints.sh
@@ -8,7 +8,7 @@ rm -rf "$ENDPOINTS_TARGET_PATH"
 mkdir "$ENDPOINTS_TARGET_PATH"
 
 cd "services/model"
-mvn clean install -Dmaven.test.skip || exit
+mvn clean install package -Dmaven.test.skip || exit
 cd ../../
 
 function compile() {
@@ -27,7 +27,7 @@ function install_endpoint() {
 
   mkdir "$target_path"
   cd "$endpoint_path" || exit
-  mvn clean install
+  mvn clean install package
   except_code
 }
 
