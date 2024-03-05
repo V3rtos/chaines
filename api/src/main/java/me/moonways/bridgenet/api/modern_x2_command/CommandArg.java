49a372113727e013312d57834ce87524bb275584
Sergey Yurchenko <gitcoder.net@gmail.com>
2024-03-05 10:39:52 +0300

    Rename CommandArgHelper.java to CommandHelper.java

--- api/src/main/java/me/moonways/bridgenet/api/modern_x2_command/CommandArg.java
+++ api/src/main/java/me/moonways/bridgenet/api/modern_x2_command/CommandArg.java
@@ -2,7 +2,7 @@ package me.moonways.bridgenet.api.modern_x2_command;
 
 import java.lang.annotation.*;
 
-@Repeatable(ComandArgHelper.class)
+@Repeatable(CommandHelper.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CommandArg {
