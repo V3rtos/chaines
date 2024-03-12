8d974a506757e9d00c7dbc698b6d8fb13d855a52
xxcoldinme <misha_stonlex@bk.ru>
2024-03-12 23:32:28 +0300

    add javadocs

--- services/model/src/main/java/me/moonways/bridgenet/model/economy/EconomyCurrencyManager.java
+++ services/model/src/main/java/me/moonways/bridgenet/model/economy/EconomyCurrencyManager.java
@@ -1,19 +1,51 @@
 package me.moonways.bridgenet.model.economy;
 
 import me.moonways.bridgenet.model.economy.bank.BankTransaction;
-import me.moonways.bridgenet.model.economy.bank.CurrencyBank;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.UUID;
 
+/**
+ * Данный управленческий класс отвечает
+ * за вызовы банковских транзакций для проведения
+ * операций над валютами относительно пользователя,
+ * который хранит данный менеджер, кешируя его данные
+ * и тем самым оптимизируя дальнейшие операции.
+ */
 public interface EconomyCurrencyManager extends Remote {
 
+    /**
+     * Выполнить запрос "ЭХО", который прослушивает
+     * актуальные данные пользователя относительно
+     * управляемой валюты и выдает в результате
+     * актуальную информацию о транзакциях пользователя.
+     */
     BankTransaction executeEcho() throws RemoteException;
 
+    /**
+     * Выполнить запрос "ОПЛАТА", который снимает
+     * определенное количество суммы со счета пользователя.
+     *
+     * @param sum - сумма, которую снимаем со счета.
+     */
     BankTransaction executePay(int sum) throws RemoteException;
 
+    /**
+     * Выполнить запрос "ПОПОЛНЕНИЕ", который пополняет на
+     * определенное количество суммы счет пользователя.
+     *
+     * @param sum - сумма, на которую пополняем счета.
+     */
     BankTransaction executeCharge(int sum) throws RemoteException;
 
+    /**
+     * Выполнить запрос "ПЕРЕВОД", который снимает
+     * определенное количество суммы со счета пользователя
+     * и переводит эту сумму другому пользователю.
+     *
+     * @param receiverId - уникальный идентификатор пользователя-получателя.
+     * @param sum - сумма, которую снимаем со счета.
+     */
     BankTransaction executeTransfer(UUID receiverId, int sum) throws RemoteException;
 }
--- services/model/src/main/java/me/moonways/bridgenet/model/economy/EconomyServiceModel.java
+++ services/model/src/main/java/me/moonways/bridgenet/model/economy/EconomyServiceModel.java
@@ -8,9 +8,25 @@ import java.util.UUID;
 
 public interface EconomyServiceModel extends RemoteService {
 
+    /**
+     * Создать новый банк по управлению указанной валютой.
+     * @param currency - валюта.
+     */
     CurrencyBank createBank(Currency currency) throws RemoteException;
 
+    /**
+     * Получить управленческий объект над операциями пользователя.
+     *
+     * @param playerId - уникальный идентификатор пользователя.
+     * @param currency - валюта, которой необходимо управлять.
+     */
     EconomyCurrencyManager getManager(UUID playerId, Currency currency) throws RemoteException;
 
+    /**
+     * Получить управленческий объект над операциями пользователя.
+     *
+     * @param playerName - имя пользователя.
+     * @param currency - валюта, которой необходимо управлять.
+     */
     EconomyCurrencyManager getManager(String playerName, Currency currency) throws RemoteException;
 }
