605074f31bc8d0703b6a0da5d873a286749cd0fb
xxcoldinme <misha_stonlex@bk.ru>
2024-05-16 19:52:08 +0300

    small refactoring code

--- testing/data/src/main/java/me/moonways/bridgenet/test/data/ExampleGreeting.java
+++ testing/data/src/main/java/me/moonways/bridgenet/test/data/ExampleGreeting.java
@@ -2,5 +2,6 @@ package me.moonways.bridgenet.test.data;
 
 public interface ExampleGreeting {
 
+    @ExampleGreetingAnnotation
     String sayHello();
 }
--- testing/units/src/test/java/me/moonways/bridgenet/test/frameworks/injection/ProxyClassInterceptionTest.java
+++ testing/units/src/test/java/me/moonways/bridgenet/test/frameworks/injection/ProxyClassInterceptionTest.java
@@ -2,20 +2,12 @@ package me.moonways.bridgenet.test.frameworks.injection;
 
 import me.moonways.bridgenet.api.inject.Inject;
 import me.moonways.bridgenet.api.proxy.AnnotationInterceptor;
-import me.moonways.bridgenet.api.proxy.MethodHandler;
-import me.moonways.bridgenet.api.proxy.MethodInterceptor;
-import me.moonways.bridgenet.api.proxy.ProxiedMethod;
 import me.moonways.bridgenet.test.data.ExampleGreeting;
 import me.moonways.bridgenet.test.data.ExampleGreetingProxy;
 import me.moonways.bridgenet.test.data.TestConst;
 import me.moonways.bridgenet.test.engine.ModernTestEngineRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,6 +20,7 @@ public class ProxyClassInterceptionTest {
     @Test
     public void test_sayHello() {
         ExampleGreeting greeting = interceptor.createProxyChecked(ExampleGreeting.class, new ExampleGreetingProxy());
+
         assertEquals(greeting.sayHello(), TestConst.Interceptor.HELLO_MESSAGE);
     }
 }
