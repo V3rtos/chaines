c63850339f6884b6481e85f2e054cc5faf105742
xxcoldinme <misha_stonlex@bk.ru>
2024-06-12 01:49:40 +0300

    fix imports in ResponsibleMessageCleanUpRunner

--- mtp/src/main/java/me/moonways/bridgenet/mtp/message/response/ResponsibleMessageCleanUpRunner.java
+++ mtp/src/main/java/me/moonways/bridgenet/mtp/message/response/ResponsibleMessageCleanUpRunner.java
@@ -2,19 +2,19 @@ package me.moonways.bridgenet.mtp.message.response;
 
 import me.moonways.bridgenet.api.inject.Inject;
 import me.moonways.bridgenet.api.autorun.persistence.AutoRunner;
-import me.moonways.bridgenet.api.autorun.persistence.Runnable;
-import me.moonways.bridgenet.api.autorun.persistence.RunningPeriod;
+import me.moonways.bridgenet.api.autorun.persistence.RunUnit;
+import me.moonways.bridgenet.api.autorun.persistence.DelayedPeriod;
 
 import java.util.concurrent.TimeUnit;
 
 @AutoRunner
-@RunningPeriod(period = 1, unit = TimeUnit.SECONDS)
+@DelayedPeriod(period = 1, unit = TimeUnit.SECONDS)
 public class ResponsibleMessageCleanUpRunner {
 
     @Inject
     private ResponsibleMessageService service;
 
-    @Runnable
+    @RunUnit
     public void run() {
         if (service != null) {
             service.cleanUp();
