34f0eb91d5b65f489d9230679960ebe9d26911e6
xxcoldinme <misha_stonlex@bk.ru>
2024-06-12 01:49:03 +0300

    fix imports in ExampleSetPropertyRunner

--- testing/data/src/main/java/me/moonways/bridgenet/test/data/management/ExampleSetPropertyRunner.java
+++ testing/data/src/main/java/me/moonways/bridgenet/test/data/management/ExampleSetPropertyRunner.java
@@ -3,8 +3,8 @@ package me.moonways.bridgenet.test.data.management;
 import lombok.extern.log4j.Log4j2;
 import me.moonways.bridgenet.api.inject.Inject;
 import me.moonways.bridgenet.api.autorun.persistence.AutoRunner;
-import me.moonways.bridgenet.api.autorun.persistence.Runnable;
-import me.moonways.bridgenet.api.autorun.persistence.RunningPeriod;
+import me.moonways.bridgenet.api.autorun.persistence.RunUnit;
+import me.moonways.bridgenet.api.autorun.persistence.DelayedPeriod;
 
 import java.util.Properties;
 import java.util.UUID;
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 @Log4j2
 @AutoRunner
-@RunningPeriod(period = 500, unit = TimeUnit.MILLISECONDS)
+@DelayedPeriod(period = 500, unit = TimeUnit.MILLISECONDS)
 public class ExampleSetPropertyRunner {
 
     public static final String KEY = "RandomValueRunner.value";
@@ -20,7 +20,7 @@ public class ExampleSetPropertyRunner {
     @Inject
     private Properties props;
 
-    @Runnable
+    @RunUnit
     public void regenerate() {
         String value = UUID.randomUUID().toString();
 
